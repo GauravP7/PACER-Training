40d105f20d0ae3d7efa0926f454f22eec2c88c23
gaurav-uc <gauravp@unicourt.com>
2018-11-16 11:57:48 +0530

    Improved the steps

--- pacer_scrape.py
+++ pacer_scrape.py
@@ -8,34 +8,34 @@ import urllib2
 '''
 Steps involed in the program for retrieving the data from PACER website:
 
-Step-1 of 9: Hit the query page of the PACER training site.
-			 URL: https://dcecf.psc.uscourts.gov/cgi-bin/iquery.pl
+Step-1 of 10: Hit the query page of the PACER training site.
+			  URL: https://dcecf.psc.uscourts.gov/cgi-bin/iquery.pl
 
-Step-2 of 9: Enter the credentials provided by the training website and login.
-			 Username="tr1234", Password="Pass!234"
-			 And be redirected to the page with case information.
+Step-2 of 10: Enter the credentials provided by the training website and login.
+			  Username="tr1234", Password="Pass!234"
+			  And be redirected to the page with case information.
 
-Step-3 of 9: Hit the URL: https://dcecf.psc.uscourts.gov/cgi-bin/iquery.pl
-			 to enter the data for querying. 
+Step-3 of 10: Hit the URL: https://dcecf.psc.uscourts.gov/cgi-bin/iquery.pl
+			  to enter the data for querying. 
 
-Step-4 of 9: Enter the values "1/1/2007" and "10/1/2007" 
-			 in the "Filed Date" column and run the query.		
+Step-4 of 10: Enter the values "1/1/2007" and "10/1/2007" 
+			  in the "Filed Date" column and run the query.		
 
-Step-4 of 9: We will be redirected to the page that contains the case realted information.
-			 Get the URL of that page.
+Step-5 of 10: We will be redirected to the page that contains the case related information.
+			  Get the URL of that page.
 
-Step-5 of 9: Hit the URL from Step-4.
+Step-6 of 10: Hit the URL from Step-5.
 
-Step-6 of 9: Save the Web page (HTML content) in a folder and display the path of the file.
+Step-7 of 10: Save the Web page (HTML content) in a folder and display the path of the file.
 
-Step-7 of 9: Open each of the links of Case Number from URL in Step-4 and 
-			 fetch the case information such as Case Number, parties involved, 
-			 Case filed and terminated dates.
+Step-8 of 10: Open each of the links of Case Number from URL in Step-4 and 
+			  fetch the case information such as Case Number, parties involved, 
+			  Case filed and terminated dates.
 		
-Step-8 of 9: Fetch the data related to the additional info and 
-			 create the appropriate JSON data structure.
+Step-9 of 10: Fetch the data related to the additional info and 
+			  create the appropriate JSON data structure.
 		
-Step-9 of 9: Logout
+Step-10 of 10: Logout
 '''
 	
 def login_into_pacer():
