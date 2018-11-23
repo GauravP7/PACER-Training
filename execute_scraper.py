dfe4a0eb7a1f174d0d285f8aff048674ecb4da90
gaurav-uc <gauravp@unicourt.com>
2018-11-23 13:59:29 +0530

    Revised steps and refactored function calls

--- execute_scraper.py
+++ execute_scraper.py
@@ -4,52 +4,24 @@ import pacer_scrape as pacer_scraper
 #Instantiate the PacerScrape class
 pacer_scraper_obj = pacer_scraper.PacerScrape()
 
-#Get the get_opener
-opener = pacer_scraper_obj.get_opener()
+# [ Step 2 of 7 ]	:	Login to training website.
+#  						Get the appropriate cookie value from the page we get after login.
+cookie_value = pacer_scraper_obj.login_pacer()	#Fetch the cookie  value for validating login
 
-#####################################################################################################
-#Step-1 of 8: Hit the cookie page of the PACER training site.                                       #
-#			 URL: https://dcecf.psc.uscourts.gov/cgi-bin/login.pl?logout 							#
-#####################################################################################################
-encoded_login_credentials = pacer_scraper_obj.login_pacer(opener)
-
-#####################################################################################################
-#Step-2 of 8: Login by entering the credentials provided by the training website.                   #
-#			 Get the appropriate cookie value from the page we get after login.                     #
-#####################################################################################################
-cookie_value = pacer_scraper_obj.get_cookie_value(opener, encoded_login_credentials)
-
-#####################################################################################################
-#Step 3 of 8: Append the cookie value to the opener header.                                         #
-#####################################################################################################
-pacer_scraper_obj.append_opener_header(opener, cookie_value)
-
-#####################################################################################################
-#Step-4 of 8: Validate PACER login by checking appropriate fields.                                  #
-#####################################################################################################
+# [ Step 3 of 7 ]	:	Validate the login.
 is_login_validate_success = pacer_scraper_obj.validate_login_success(cookie_value)
 
-#Terminate the program if validation is unsuccessful
-if not is_login_validate_success:
+if not is_login_validate_success:	#Terminate the program if validation is unsuccessful
 	pacer_scraper_obj.terminate_with_error_message()
 
-#####################################################################################################
-#Step-5 of 8: Hit the URL: https://dcecf.psc.uscourts.gov/cgi-bin/iquery.pl							#
-#			  and enter the data for querying.                                                      #
-#####################################################################################################
-case_details_page_contents = pacer_scraper_obj.hit_query_page(opener)
+# [ Step 4 of 7 ]	:	Query as per the input criteria.
+case_details_page_contents = pacer_scraper_obj.get_case_details_page_contents()
 
-#####################################################################################################
-#Step-6 of 8: Save the Web page (HTML content) in a folder and save it's the path.                  #
-#####################################################################################################
-page_path = pacer_scraper_obj.save_webpage_and_get_page_path(case_details_page_contents)
+# [ Step 5 of 7 ]	:	Save the Web page (HTML content) in a folder.
+page_path = pacer_scraper_obj.save_webpage_with_case_details(case_details_page_contents)
 
-#####################################################################################################
-#Step-7 of 8: Save single case information including the additional information of that case.       #
-#####################################################################################################
-pacer_scraper_obj.save_case_details(opener, case_details_page_contents)
+# [ Step 6 of 7 ]	:	Save the case details.
+pacer_scraper_obj.save_case_details(case_details_page_contents)
 
-#####################################################################################################
-#Step-8 of 8: Logout from the website.																#
-#####################################################################################################
-pacer_scraper_obj.logout(opener)
+# [ Step 7 of 7 ]	:	Logout from the website.
+pacer_scraper_obj.logout()
--- pacer_scrape.py
+++ pacer_scrape.py
@@ -1,40 +1,37 @@
 #!/py-virtualenv/ENV/bin/python
+import sys
 import urllib
 import urllib2
 import re
 import cookielib
 from bs4 import BeautifulSoup
 
-#####################################################################################################
-#Step-1 of 8: Hit the cookie page of the PACER training site.                                       #
-#			 URL: https://dcecf.psc.uscourts.gov/cgi-bin/login.pl?logout 							#
-#####################################################################################################
-#Step-2 of 8: Login by entering the credentials provided by the training website.                   #
-#			 Get the appropriate cookie value from the page we get after login.                     #
-#####################################################################################################
-#Step 3 of 8: Append the cookie value to the opener header.                                         #
-#####################################################################################################
-#Step-4 of 8: Validate PACER login by checking appropriate fields.                                  #
-#####################################################################################################
-#Step-5 of 8: Hit the URL: https://dcecf.psc.uscourts.gov/cgi-bin/iquery.pl							#
-#			  and enter the data for querying.                                                      #
-#####################################################################################################
-#Step-6 of 8: Save the Web page (HTML content) in a folder and save it's the path.                  #
-#####################################################################################################
-#Step-7 of 8: Save single case information including the additional information of that case.       #
-#####################################################################################################
-#Step-8 of 8: Logout from the website.																#
-#####################################################################################################
+# [ Step 1 of 7 ]	:	Hit the first page of PACER training site to set the cookie.
+#  						URL: https://dcecf.psc.uscourts.gov/cgi-bin/login.pl?logout
+#
+# [ Step 2 of 7 ]	:	Login to training website.
+#  						Get the appropriate cookie value from the page we get after login.
+#
+# [ Step 3 of 7 ]	:	Validate the login.
+#
+# [ Step 4 of 7 ]	:	Query as per the input criteria.
+#
+# [ Step 5 of 7 ]	:	Save the Web page (HTML content) in a folder.
+#
+# [ Step 6 of 7 ]	:	Save the case details.
+#
+# [ Step 7 of 7 ]	:	Logout from the website.
 
 class SearchCriteria():
-	'''
-	Summary
-	Class Name: SearchCriteria
-	Inherits: None
-	Description: Class holds the search criteria as mentioned in the Schema.
-	Member functions:
-			1. print_search_criteria()
-	'''
+	"""
+		Class holds the search criteria as mentioned in the Schema.
+
+		Inherits:
+				None
+
+		Member functions:
+				1. print_search_criteria()
+	"""
 
 	def __init__(self):
 		self.user_type = ''
@@ -55,12 +52,12 @@ class SearchCriteria():
 
 
 	def print_search_criteria(self):
-		'''
-		Summary
-		Method Name: print_search_criteria
-		Description: Prints all the search criteria used in for Querying the data from the website
-		Arguments: self
-		'''
+		"""
+			Prints all the search criteria used in for Querying the data from the website
+
+			Arguments:
+					self
+		"""
 
 		print "***************************************************"
 		print "Search Criteria are:"
@@ -83,102 +80,40 @@ class SearchCriteria():
 
 
 class PacerScrape():
-	'''
-	Summary
-	Class Name: PacerScrape
-	Inherits: None
-	Description: Class is used to scrape the case related data from the PACER training website.
-	Member functions:
-			1.  get_opener()
-			2.  get_cookie_value(opener, encoded_credentials)
-			3.  get_url_random_numbers(query_page_contents)
-			4.  login_pacer(username, password)
-			5.  append_opener_header(opener, cookie_value)
-			6.  validate_login_success(pacer_session_cookie_value)
-			7.  hit_query_page(opener)
-			8.  save_webpage_and_get_page_path(page_contents)
-			9.  save_case_details(opener, case_details_page_contents)
-			10. logout(opener)
-			11. terminate_with_error_message()
-	'''
-	def get_opener(self):
-		'''
-		Summary
-		Method Name: get_opener
-		Description: Initialize and install the opener
-		Arguments: self
-		'''
+	"""
+		Class is used to scrape the case related data from the PACER training website.
 
-		opener = urllib2.build_opener()
-		urllib2.install_opener(opener)
-		return opener
+		Inherits:
+				None
 
-	def get_cookie_value(self, opener, encoded_credentials):
-		'''
-		Summary
-		Method Name: get_cookie_value
-		Description: Get the value of the cookie named 'PacerSession' from the HTML page
-					 on successful loginself.
-					 Terminate the program otherwiseself.
-		Arguments: self, opener, encoded_credentials
-		'''
+		Member functions:
+				1.  login_pacer(self)
+				2.  validate_login_success(self, pacer_session_cookie_value)
+				3.	get_case_details_page_contents(self)
+				4.	save_webpage_with_case_details(self, page_contents)
+				5.	save_webpage_with_case_details(self, page_contents)
+				6.	save_case_details(self, case_details_page_contents)
+				7.	logout(self)
+				8.	terminate_with_error_message(self)
+	"""
 
-		login_page = 'https://dcecf.psc.uscourts.gov/cgi-bin/login.pl?logout'
+	def __init__(self):
+		"""
+			Default constructor for setting up the opener
+		"""
+		self.opener = urllib2.build_opener()
+		urllib2.install_opener(self.opener)
 
-		try:
-			login_page_request = urllib2.Request(login_page)
+	def login_pacer(self):
+		"""
+			Used to login into the PACER training website and make related function calls.
 
-			login_page_response = opener.open(login_page_request , encoded_credentials)
-			login_page_contents = login_page_response.read()
+			Arguments:
+					self, username, password
 
-			login_page_soup = BeautifulSoup(login_page_contents, 'html.parser')
-			'''
-			The 'html.parser' is unnecessay since it is Default to BeautifulSoup.
-			However, it is better to use it to avoid the WARNING.
-			'''
-			#Extract cookie value
-			script_value = login_page_soup.script.get_text()
-
-			#print script_value
-			pacer_session_cookie_value = re.split( 'PacerSession |, |;', re.findall(r'"(.*?)"', script_value)[0])[0]
-
-			return pacer_session_cookie_value
-
-		except:
-			self.terminate_with_error_message()
-
-	def get_url_random_numbers(self, query_page_contents):
-		'''
-		Summary
-		Method Name: get_url_random_numbers
-		Description: Return the random numbers used in the URL
-		Arguments: self, query_page_contents
-		'''
-
-		MINIMUM_FORM_NUMBER_SIZE = 5
-		#Extract random numbers
-		query_page_soup = BeautifulSoup(query_page_contents, 'html.parser')
-		form_action_value = query_page_soup.find('form').get('action')
-		action_content = re.findall(r"[0-9]*", form_action_value)
-
-		'''
-		#Extract only the number part
-		for form_number in action_content:
-			if form_number.isdigit() and len(form_number) >= MINIMUM_FORM_NUMBER_SIZE:
-				required_form_number = form_number
-		'''
-		required_form_number = [form_number for form_number in action_content if form_number.isdigit() and len(form_number) >= MINIMUM_FORM_NUMBER_SIZE][0]
-
-		return required_form_number
-
-
-	def login_pacer(self, opener):
-		'''
-		Summary
-		Method Name: login_pacer
-		Description: Used to login into the PACER training website and make related function calls.
-		Arguments: self, username, password
-		'''
+			Returns:
+					pacer_session_cookie_value
+		"""
 
 		username = 'tr1234'
 		password = 'Pass!234'
@@ -186,48 +121,83 @@ class PacerScrape():
 		credentials = {'login': username, 'key': password}
 		encoded_login_credentials = urllib.urlencode(credentials)
 
-		return encoded_login_credentials
+		login_page = 'https://dcecf.psc.uscourts.gov/cgi-bin/login.pl?logout'
 
-	def append_opener_header(self, opener, cookie_value):
+		pacer_session_cookie_value = ""
 
-		#Get the session cookie value
-		pacer_session_cookie_value = cookie_value
+		# [ Step 1 of 7 ]	:	Hit the cookie page of the PACER training site.
+		#  						URL: https://dcecf.psc.uscourts.gov/cgi-bin/login.pl?logout
+		try:
+			login_page_request = urllib2.Request(login_page)
+
+			login_page_response = self.opener.open(login_page_request , encoded_login_credentials)
+			login_page_contents = login_page_response.read()
+
+			login_page_soup = BeautifulSoup(login_page_contents, 'html.parser')
+
+			#The 'html.parser' is unnecessay since it is Default to BeautifulSoup.
+			#However, it is better to use it to avoid the WARNING.
+
+			#Extract cookie value
+			script_value = login_page_soup.script.get_text()
+
+			#print script_value
+			pacer_session_cookie_value = re.split( 'PacerSession |, |;', re.findall(r'"(.*?)"', script_value)[0])[0]
+
+		except:
+			print sys._getframe().f_code.co_name
+			self.terminate_with_error_message()
 
 		#Add the required headers
-		opener.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36')]
+		self.opener.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36')]
 
-		opener.addheaders.append(('Referer','https://dcecf.psc.uscourts.gov/cgi-bin/ShowIndex.pl'))
+		self.opener.addheaders.append(('Referer','https://dcecf.psc.uscourts.gov/cgi-bin/ShowIndex.pl'))
 
-		opener.addheaders.append(('Cookie', pacer_session_cookie_value ))
+		self.opener.addheaders.append(('Cookie', pacer_session_cookie_value ))
+
+		return pacer_session_cookie_value
 
 	def validate_login_success(self, pacer_session_cookie_value):
-		'''
-		Summary
-		Method Name: login_validate_success
-		Description: Returns True if login is successful. returns False otherwise.
-		Arguments: self, username, password
-		'''
+		"""
+			Returns True if login is successful. returns False otherwise.
+
+			Arguments:
+					self, username, password
+		"""
 
 		if pacer_session_cookie_value is None or pacer_session_cookie_value == '':
 			return False
 
 		return True
 
-	def hit_query_page(self, opener):
-		'''
-		Summary
-		Method Name: hit_query_page
-		Description: Hits the query URL and returns the case details page
-					 after querying with appropriate search criteria
-		Arguments: self, username, password
-		'''
+	def get_case_details_page_contents(self):
+		"""
+			 Hits the query URL and returns the case details page
+			 after querying with appropriate search criteria
+
+			 Arguments:
+			 		self, username, password
+
+			Returns:
+					case_details_page_contents
+
+		"""
 
 		query_page_url = 'https://dcecf.psc.uscourts.gov/cgi-bin/iquery.pl'
-		query_page_response = opener.open(query_page_url)
+		query_page_response = self.opener.open(query_page_url)
 		query_page_contents = query_page_response.read()
 
 		#By here we get the contents of the "Query" page
-		required_form_number = self.get_url_random_numbers(query_page_contents)
+		MINIMUM_FORM_NUMBER_SIZE = 5
+		#Extract random numbers
+		query_page_soup = BeautifulSoup(query_page_contents, 'html.parser')
+		form_action_value = query_page_soup.find('form').get('action')
+		action_content = re.findall(r"[0-9]*", form_action_value)
+
+		# Extract only the number part
+		for form_number in action_content:
+			if form_number.isdigit() and len(form_number) >= MINIMUM_FORM_NUMBER_SIZE:
+				required_form_number = form_number
 
 		#Now we start hitting the content page
 		data_page_url = "https://dcecf.psc.uscourts.gov/cgi-bin/iquery.pl?" + required_form_number + "-L_1_0-1"
@@ -254,19 +224,22 @@ class PacerScrape():
 
 		query_parameters_encoded = urllib.urlencode(query_parameters)
 		query_request = urllib2.Request(data_page_url, query_parameters_encoded)
-		query_response = opener.open(query_request)
+		query_response = self.opener.open(query_request)
 
 		case_details_page_contents = query_response.read()
 
 		return case_details_page_contents
 
-	def save_webpage_and_get_page_path(self, page_contents):
-		'''
-		Summary
-		Method Name: save_webpage_and_get_page_path
-		Description: Saves the HTML code of the Webpage containing the Case details
-		Arguments: self, page_contents
-		'''
+	def save_webpage_with_case_details(self, page_contents):
+		"""
+			Saves the HTML code of the Webpage containing the Case details
+
+			Arguments:
+					self, page_contents
+
+			Returns:
+					page_path
+		"""
 
 		saved_webpage_file_path = '/home/mis/Training/Contents/'
 		saved_webpage_file_name = 'case_details.html'
@@ -279,13 +252,13 @@ class PacerScrape():
 
 		return page_path
 
-	def save_case_details(self, opener, case_details_page_contents):
-		'''
-		Summary
-		Method Name: save_case_details
-		Description: Saves the case details and Additional info as mentioned in the Schema
-		Arguments: self, opener, case_details_page_contents
-		'''
+	def save_case_details(self, case_details_page_contents):
+		"""
+			Saves the case details and Additional info as mentioned in the Schema
+
+			Arguments:
+					self, opener, case_details_page_contents
+		"""
 
 		try:
 			case_details_page_soup =  BeautifulSoup(case_details_page_contents, 'html.parser')
@@ -302,24 +275,24 @@ class PacerScrape():
 				for td in t_data:
 					if '-' in td.text:
 						case_number = td.text
-						print 'case_number:\t', case_number
+						print "case_number:\t", case_number
 
 					elif 'filed' in td.text:
 						required_date = td.text
 						try:
 							case_filed_date = required_date.split()[1]
 							case_closed_date = required_date.split()[3]
-						except:
+						except:	#Hnadling exceptions for cases without closed date
 							case_closed_date = ''
 
-						print 'case_filed_date:\t', case_filed_date
-						print 'case_closed_date:\t', case_closed_date
+						print "case_filed_date:\t", case_filed_date
+						print "case_closed_date:\t", case_closed_date
 						case_details_count += 1
 						print "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
 
 					else:
 						parties_involved = td.text
-						print 'parties_involved:\t', parties_involved
+						print "parties_involved:\t", parties_involved
 
 			print "***************************************************"
 
@@ -332,7 +305,7 @@ class PacerScrape():
 			for case_detail in case_details_url_list:
 				base_url = 'https://dcecf.psc.uscourts.gov/cgi-bin'
 				current_case_url = case_detail['href'] #Get the link of a particular case
-				current_case_response = opener.open(base_url + '/' + current_case_url)
+				current_case_response = self.opener.open(base_url + '/' + current_case_url)
 				current_case_contents = current_case_response.read()
 
 
@@ -360,25 +333,25 @@ class PacerScrape():
 			print "Error occured when saving case details"
 			print "Failed to save some or all of the case details"
 
-	def logout(self, opener):
-		'''
-		Summary
-		Method Name: logout
-		Description: Logout from the website
-		Arguments: self, opener
-		'''
+	def logout(self):
+		"""
+			Logout from the website
+
+			Arguments:
+					self, opener
+		"""
 
 		logout_page_url = "https://dcecf.psc.uscourts.gov/cgi-bin/login.pl?logout"
-		logout_response = opener.open(logout_page_url)
+		logout_response = self.opener.open(logout_page_url)
 		logout_page_contents = logout_response.read()
 
 	def terminate_with_error_message(self):
-		'''
-		Summary
-		Method Name: terminate_with_error_message
-		Description: Terminate the program with upon unsuccessful login
-		Arguments: self
-		'''
+		"""
+			Terminate the program with upon unsuccessful login
+
+			Arguments:
+					self
+		"""
 
 		print "The program is terminated since the login was unsuccessful."
 		print "Please check the credentials or your internet connection and try again"
