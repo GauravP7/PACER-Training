18ad117675f98f1cf016dd7ee50a126b66c88b0b
gaurav-uc <gauravp@unicourt.com>
2018-11-24 11:24:49 +0530

    Tuple unpacking feature and removal of try-except blocks

--- execute_scraper.py
+++ execute_scraper.py
@@ -3,10 +3,10 @@ import pacer_scrape as pacer_scraper
 #Instantiate the PacerScrape class
 pacer_scraper_obj = pacer_scraper.PacerScrape()
 
-# [ Step 1 of 8 ] : Hit the first page of PACER training site and Login.
+# [ Step 1 of 9 ] : Hit the first page of PACER training site and Login.
 login_page_contents = pacer_scraper_obj.login_pacer()
 
-# [ Step 2 of 8 ] : Validate the Login.
+# [ Step 2 of 9 ] : Validate the Login.
 is_login_validate_success = pacer_scraper_obj.validate_login_success(login_page_contents)
 
 if not is_login_validate_success:	#Terminate the program if validation is unsuccessful
@@ -15,27 +15,64 @@ if not is_login_validate_success:	#Terminate the program if validation is unsucc
 else:
 	print "Login successful"
 
-# [ Step 3 of 8 ] : Parse the contents and get cookie.
-cookie_value = pacer_scraper_obj.get_cookie_value(login_page_contents)
+# [ Step 3 of 9 ] : Parse the contents and get cookie.
+pacer_scraper_obj.get_cookie_value(login_page_contents)
 
-# [ Step 4 of 8 ] : Query as per the input criteria.
+# [ Step 4 of 9 ] : Query as per the input criteria.
 case_details_page_contents = pacer_scraper_obj.get_case_details_page_contents()
 
-# [ Step 5 of 8 ] : Save the Web page (HTML content) in a folder.
+# [ Step 5 of 9 ] : Save the Web page (HTML content) in a folder.
 page_path = pacer_scraper_obj.save_webpage_with_case_details(case_details_page_contents)
 
-# [ Step 6 of 8 ] : Print the page path.
+# [ Step 6 of 9 ] : Print the page path.
 print "page_path:\t", page_path
 
-# [ Step 7 of 8 ] : Print the case details.
-pacer_scraper_obj.parse_case_details(case_details_page_contents)
+# [ Step 7 of 9 ] : Print the Search Criteria.
+#Print the search criteria
+search_criteria = pacer_scraper.SearchCriteria()
+print "Search Criteria are:"
+print "user_type:\t", search_criteria.user_type
+print "all_case_ids:\t", search_criteria.all_case_ids
+print "case_number:\t", search_criteria.case_number
+print "case_status:\t", search_criteria.case_status
+print "from_filed_date:\t", search_criteria.from_filed_date
+print "to_filed_date:\t", search_criteria.to_filed_date
+print "from_last_entry_date:\t", search_criteria.from_last_entry_date
+print "to_last_entry_date:\t", search_criteria.to_last_entry_date
+print "nature_of_suit:\t", search_criteria.nature_of_suit
+print "cause_of_action:\t", search_criteria.cause_of_action
+print "last_name:\t", search_criteria.last_name
+print "first_name:\t", search_criteria.first_name
+print "middle_name:\t", search_criteria.middle_name
+print "type:\t", search_criteria.type
+print "exact_matches_only:\t", search_criteria.exact_matches_only
 
-case_details_file_object = open("parsed_case_details.txt", "r")
-case_detail_contents = case_details_file_object.read()
+# [ Step 8 of 9 ] : Print the case details.
+#Get the case revevent information
+case_details_with_additional_info_tuple = pacer_scraper_obj.get_case_details(case_details_page_contents)
 
-print "The case details are:"
+#Segregate the tuple to get the case details and additional info
+case_details_list = case_details_with_additional_info_tuple[0]
+additional_info_json = case_details_with_additional_info_tuple[1]
 
-print case_detail_contents
+#Print the case details
+print "The addtional info are:"
+for case_details in  case_details_list:
+	case_number = case_details[0]
+	parties_involved = case_details[1]
+	case_filed_date = case_details[2]
+	case_closed_date = case_details[3]
+	print "case_number:\t", case_number
+	print "parties_involved:\t", parties_involved
+	print "case_filed_date:\t", case_filed_date
+	print "case_closed_date:\t", case_closed_date
+	print "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
 
-# [ Step 8 of 8 ] : Logout from the website.
+#Print the additional information of the cases
+print "The addtional info are:"
+for additional_info in additional_info_json:
+	print additional_info
+	print "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+# [ Step 9 of 9 ] : Logout from the website.
 pacer_scraper_obj.logout()
--- pacer_scrape.py
+++ pacer_scrape.py
@@ -5,29 +5,29 @@ import urllib2
 import re
 from bs4 import BeautifulSoup
 
-# [ Step 1 of 8 ] : Hit the first page of PACER training site and Login.
+# [ Step 1 of 9 ] : Hit the first page of PACER training site and Login.
 #
-# [ Step 2 of 8 ] : Validate the Login.
+# [ Step 2 of 9 ] : Validate the Login.
 #
-# [ Step 3 of 8 ] : Parse the contents and get cookie.
+# [ Step 3 of 9 ] : Parse the contents and get cookie.
 #
-# [ Step 4 of 8 ] : Query as per the input criteria.
+# [ Step 4 of 9 ] : Query as per the input criteria.
 #
-# [ Step 5 of 8 ] : Save the Web page (HTML content) in a folder.
+# [ Step 5 of 9 ] : Save the Web page (HTML content) in a folder.
 #
-# [ Step 6 of 8 ] : Print the page path.
+# [ Step 6 of 9 ] : Print the page path.
 #
-# [ Step 7 of 8 ] : Print the case details.
+# [ Step 7 of 9 ] : Print the Search Criteria.
 #
-# [ Step 8 of 8 ] : Logout from the website.
+# [ Step 8 of 9 ] : Print the case details.
+#
+# [ Step 9 of 9 ] : Logout from the website.
 
 class SearchCriteria():
 	"""
 		Class holds the search criteria as mentioned in the Schema.
-
 		Inherits:
 				None
-
 		Member functions:
 				1. print_search_criteria()
 	"""
@@ -51,41 +51,11 @@ class SearchCriteria():
 		self.type = ''
 		self.exact_matches_only = 0
 
-	def print_search_criteria(self):
-		"""
-			Prints all the search criteria used in for Querying the data from the website
-
-			Arguments:
-					self
-		"""
-
-		print "***************************************************"
-		print "Search Criteria are:"
-		print "user_type:\t", self.user_type
-		print "all_case_ids:\t", self.all_case_ids
-		print "case_number:\t", self.case_number
-		print "case_status:\t", self.case_status
-		print "from_filed_date:\t", self.from_filed_date
-		print "to_filed_date:\t", self.to_filed_date
-		print "from_last_entry_date:\t", self.from_last_entry_date
-		print "to_last_entry_date:\t", self.to_last_entry_date
-		print "nature_of_suit:\t", self.nature_of_suit
-		print "cause_of_action:\t", self.cause_of_action
-		print "last_name:\t", self.last_name
-		print "first_name:\t", self.first_name
-		print "middle_name:\t", self.middle_name
-		print "type:\t", self.type
-		print "exact_matches_only:\t", self.exact_matches_only
-		print "***************************************************"
-
-
 class PacerScrape():
 	"""
 		Class is used to scrape the case related data from the PACER training website.
-
 		Inherits:
 				None
-
 		Member functions:
 				1. login_pacer(self)
 				2. get_cookie_value(self, login_page_contents)
@@ -100,7 +70,6 @@ class PacerScrape():
 	def __init__(self):
 		"""
 			Default constructor
-
 			Tasks:
 			 1. sets up the opener
 			 2. Initializes login credentials
@@ -120,59 +89,51 @@ class PacerScrape():
 	def login_pacer(self):
 		"""
 			Used to login into the PACER training website and make related function calls.
-
 			Arguments:
 					self
-
 			Returns:
 					pacer_session_cookie_value
 		"""
 
 		credentials = {'login': self.username, 'key': self.password}
 		encoded_login_credentials = urllib.urlencode(credentials)
-
 		login_page = 'https://dcecf.psc.uscourts.gov/cgi-bin/login.pl?logout'
-
 		login_page_request = urllib2.Request(login_page)
-
 		login_page_response = self.opener.open(login_page_request , encoded_login_credentials)
 		login_page_contents = login_page_response.read()
-
 		return login_page_contents
 
 	def get_cookie_value(self, login_page_contents):
-
+		"""
+			Used to get the cookie value.
+			Arguments:
+					self, login_page_contents
+		"""
 		login_page_soup = BeautifulSoup(login_page_contents, 'html.parser')
 
 		#The 'html.parser' is unnecessay since it is Default to BeautifulSoup.
 		#However, it is better to use it to avoid the WARNING.
-
 		#Extract cookie value
 		script_value = login_page_soup.script.get_text()
 
-		#print script_value
+		#Example
+		#Input: "PacerSession=JjRYJFwy9L7sFOycTEI3I18YOhazqPx7OEdgtw235ClZlnQMrTdgivMC6FQ5iHAUhfu0xECTDmcCrrjzH4CFqBKq4RhbvKzrG7lcFpNL72OwB76QicEUFSIKY3DSoEiz;"
+		#Output: JjRYJFwy9L7sFOycTEI3I18YOhazqPx7OEdgtw235ClZlnQMrTdgivMC6FQ5iHAUhfu0xECTDmcCrrjzH4CFqBKq4RhbvKzrG7lcFpNL72OwB76QicEUFSIKY3DSoEiz
 		self.pacer_session_cookie_value = re.split( 'PacerSession |, |;', re.findall(r'"(.*?)"', script_value)[0])[0]
 
-
-		#Add the required headers
+		#Add the required headers to the opener
 		self.opener.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36')]
-
 		self.opener.addheaders.append(('Referer','https://dcecf.psc.uscourts.gov/cgi-bin/ShowIndex.pl'))
-
 		self.opener.addheaders.append(('Cookie', self.pacer_session_cookie_value ))
-
-		return self.pacer_session_cookie_value
 
 	def validate_login_success(self, login_page_contents):
 		"""
 			Returns True if login is successful. returns False otherwise.
-
 			Arguments:
-					self, username, password
+					self, login_page_contents
 		"""
 
 		login_page_soup = BeautifulSoup(login_page_contents, 'html.parser')
-
 		login_page_h3_tags = login_page_soup.find_all('h3')
 
 		for h3_tag in login_page_h3_tags:
@@ -185,10 +146,8 @@ class PacerScrape():
 		"""
 			 Hits the query URL and returns the case details page
 			 after querying with appropriate search criteria
-
 			 Arguments:
-			 		self, username, password
-
+			 		self
 			Returns:
 					case_details_page_contents
 
@@ -197,9 +156,8 @@ class PacerScrape():
 		query_page_url = 'https://dcecf.psc.uscourts.gov/cgi-bin/iquery.pl'
 		query_page_response = self.opener.open(query_page_url)
 		query_page_contents = query_page_response.read()
+		minimum_size_of_form_number = 5
 
-		#By here we get the contents of the "Query" page
-		MINIMUM_FORM_NUMBER_SIZE = 5
 		#Extract random numbers
 		query_page_soup = BeautifulSoup(query_page_contents, 'html.parser')
 		form_action_value = query_page_soup.find('form').get('action')
@@ -207,16 +165,12 @@ class PacerScrape():
 
 		# Extract only the number part
 		for form_number in action_content:
-			if form_number.isdigit() and len(form_number) >= MINIMUM_FORM_NUMBER_SIZE:
+			if form_number.isdigit() and len(form_number) >= minimum_size_of_form_number:
 				required_form_number = form_number
 
 		#Now we start hitting the content page
 		data_page_url = "https://dcecf.psc.uscourts.gov/cgi-bin/iquery.pl?" + required_form_number + "-L_1_0-1"
-
 		search_criteria = SearchCriteria()
-
-		#Print the search criteria
-		search_criteria.print_search_criteria()
 
 		#The parameters to be encoded and sent as a search criteria
 		query_parameters = {
@@ -236,18 +190,14 @@ class PacerScrape():
 		query_parameters_encoded = urllib.urlencode(query_parameters)
 		query_request = urllib2.Request(data_page_url, query_parameters_encoded)
 		query_response = self.opener.open(query_request)
-
 		case_details_page_contents = query_response.read()
-
 		return case_details_page_contents
 
 	def save_webpage_with_case_details(self, page_contents):
 		"""
 			Saves the HTML code of the Webpage containing the Case details
-
 			Arguments:
 					self, page_contents
-
 			Returns:
 					page_path
 		"""
@@ -258,104 +208,76 @@ class PacerScrape():
 		file_object = open(page_path, "w+")
 		file_object.write(page_contents)
 		file_object.close()
-		print "***************************************************"
-
 		return page_path
 
-	def parse_case_details(self, case_details_page_contents):
+	def get_case_details(self, case_details_page_contents):
 		"""
 			Saves the case details and Additional info as mentioned in the Schema
-
 			Arguments:
 					self, opener, case_details_page_contents
 		"""
 
-		parsed_case_details_file_object = open("parsed_case_details.txt","r+")
-
+		case_details_tuple = ()
+		case_details_list = []
 		case_details_page_soup =  BeautifulSoup(case_details_page_contents, 'html.parser')
-
-		table_contents = case_details_page_soup.find_all('tr')[:-4570]
 
 		#The last 6 rows contain the irrelevant information
 		#Hence they are discarded
-
+		table_contents = case_details_page_soup.find_all('tr')[:-6]
 		for t_rows in table_contents:
 			t_data = t_rows.find_all('td')
 			case_details_count = 0
-
 			for td in t_data:
 				if '-' in td.text:
 					case_number = td.text
-					#print "case_number:\t", case_number
-					parsed_case_details_file_object.write("case_number:\t" + str(case_number) + "\n")
-
 				elif 'filed' in td.text:
 					required_date = td.text
-					try:
-						case_filed_date = required_date.split()[1]
-						parsed_case_details_file_object.write("case_filed_date:\t" + str(case_filed_date) + "\n")
+					case_filed_date = required_date.split()[1]
 
+					#Check for cases without closed date
+					if len(required_date.split()) > 3:
 						case_closed_date = required_date.split()[3]
-						parsed_case_details_file_object.write("case_closed_date:\t" + str(case_closed_date) + "\n")
-					except:	#Hnadling exceptions for cases without closed date
+					else:
 						case_closed_date = ''
-						parsed_case_details_file_object.write("case_closed_date:\t" + str(case_closed_date) + "\n")
+					case_details_tuple = ()
+					case_details_tuple = (case_number, parties_involved, case_filed_date, case_closed_date)
 
-					#print "case_filed_date:\t", case_filed_date
-					#print "case_closed_date:\t", case_closed_date
+					#Tuple packing
+					case_details_list.append(case_details_tuple)
 					case_details_count += 1
-					parsed_case_details_file_object.write("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n")
-					#print "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-
 				else:
 					parties_involved = td.text
-					#print "parties_involved:\t", parties_involved
-					parsed_case_details_file_object.write("parties_involved:\t" + str(parties_involved) + "\n")
 
-		print "***************************************************"
-
+		#Get the additional info of all the cases
 		#Get links of all the cases
 		case_details_url_list = case_details_page_soup.select('td a')
-		case_details_dict = {}
-		case_details_list = []
+		additional_info_list = []
 		additional_info_count = 0
-
 		for case_detail in case_details_url_list:
 			base_url = 'https://dcecf.psc.uscourts.gov/cgi-bin'
 			current_case_url = case_detail['href'] #Get the link of a particular case
 			current_case_response = self.opener.open(base_url + '/' + current_case_url)
 			current_case_contents = current_case_response.read()
 
-
 		    #Generate additional info
 			case_contents_soup = BeautifulSoup(current_case_contents, 'html.parser')
 			additional_info_links_list = case_contents_soup.select('td a')
 			additonal_info_json = {}
 
+		    #Create additional info JSON data structure
 			for additional_info in additional_info_links_list:
-
 				additional_info_name = additional_info.text
 				additional_info_link = additional_info['href']
 				additonal_info_json[additional_info_name] = additional_info_link
-
 			additional_info_count += 1
-
-			parsed_case_details_file_object.write("Additional info are:\n")
-			parsed_case_details_file_object.write("additional_info_json:\t:" + str(additonal_info_json) + "\n")
-
-			#print "Additional info are:\t"
-			#print "additional_info_json:\t", additonal_info_json
-			#print "***************************************************"
-
-			if additional_info_count >= 1:
+			additional_info_list.append(additonal_info_json)
+			if additional_info_count >= 2:
 				break
-
-		parsed_case_details_file_object.close()
+		return (case_details_list, additional_info_list)
 
 	def logout(self):
 		"""
 			Logout from the website
-
 			Arguments:
 					self, opener
 		"""
@@ -367,11 +289,10 @@ class PacerScrape():
 	def terminate_with_error_message(self):
 		"""
 			Terminate the program with upon unsuccessful login
-
 			Arguments:
 					self
 		"""
 
 		print "The program is terminated since the login was unsuccessful."
 		print "Please check the credentials or your internet connection and try again"
-		exit()
+		exit(0)
