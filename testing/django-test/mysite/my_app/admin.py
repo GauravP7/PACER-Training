86bae273345372ad5afe7e1610d9c227f2a8e34f
Gaurav Prabhu K <45057735+gaurav-uc@users.noreply.github.com>
2018-11-29 13:58:05 +0530

    Update and rename django-test/mysite/my_app/admin.py to testing/django-test/mysite/my_app/admin.py

--- testing/django-test/mysite/my_app/admin.py
+++ testing/django-test/mysite/my_app/admin.py
@@ -4,9 +4,7 @@ from my_app.models import PageContent
 from my_app.models import SearchCriteria
 from django.contrib import admin
 
-class MysiteAdmin(admin.ModelAdmin):
-    fields = ['page_content_id', 'page_path']
 admin.site.register(AdditionalInfo)
-#admin.site.register(CaseDetails)
-#admin.site.register(PageContent)
-#admin.site.register(SearchCriteria)
+admin.site.register(CaseDetails)
+admin.site.register(PageContent)
+admin.site.register(SearchCriteria)
